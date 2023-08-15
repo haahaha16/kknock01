aa3ba3d9ce7d876545bbc00bc16b1852db54c3b4
haahaha16 <lucia04lee@gmail.com>
2023-08-16 07:50:38 +0900

    error

--- HP/lib/dbconn.php
+++ HP/lib/dbconn.php
@@ -1,4 +1,16 @@
 <?php
     $conn = mysqli_connect('20.196.204.220', 'root','mysql','webDB', '8010');
+    if(!$connect){
+
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+        exit();
+    
+    }
+    
     mysqli_query($conn, "set names utf8");
 ?>
