0dc912dfe6602bf18446721db6e33db90d13d776
haahaha16 <lucia04lee@gmail.com>
2023-08-16 07:21:26 +0900

    db

--- HP/lib/dbconn.php
+++ HP/lib/dbconn.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect('20.196.204.220', 'root','mysql','webDB', '8010');
+    $conn = mysqli_connect('20.196.204.220', 'root','mysql','webDB');
     mysqli_query($conn, "set names utf8");
 ?>
