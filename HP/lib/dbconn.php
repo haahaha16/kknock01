786e18872e8f8e4f2d9baaf6a619cdf077954ea6
haahaha16 <lucia04lee@gmail.com>
2023-08-31 14:54:03 +0900

    dbcn

--- HP/lib/dbconn.php
+++ HP/lib/dbconn.php
@@ -1,14 +1,10 @@
 <?php
-    $conn = mysqli_connect('20.196.204.220', 'root','mysql','webDB', '8010', true);
+    $conn = mysqli_connect("20.196.204.220", "root","mysql","webDB");
     if(!$connect){
 
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    
-        exit();
+        echo "<p>Failure - " . mysqli_connect_error() . "</p>";
+    } else {
+      echo "<p>Success</p>";
     
     }
     
